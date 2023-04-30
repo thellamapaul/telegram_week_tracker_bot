8b35bb9e0306cef2ef9c755d12c0a30a3cdbce32
thellamapaul <liamrbriggs@gmail.com>
2023-04-30 14:20:44 -0500

    edited progress10 to use floor instead of round added progress15, added progress12, split tracker into functions

--- week_tracker_bot.py
+++ week_tracker_bot.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import telebot
+import math
 from datetime import datetime
 
 #take input of bot api like 'python bot.pi num:str'
@@ -9,13 +10,97 @@ BOT_TOKEN = sys.argv[1]
 #create bot
 bot = telebot.TeleBot(BOT_TOKEN)
 
-def progress(percent):
+#progress bar with 10 blocks with shaded empty function
+def progress10shade(percent):
     bar_len = 10
-    filled_len = int(round(bar_len * percent))
-
+    filled_len = math.floor(bar_len * percent)
     bar = '█' * filled_len + '░' * (bar_len - filled_len)
-
     return bar
+
+#progress bar with 15 blocks, partial and fully shade empty function
+def progress15semishade(percent):
+    bar_len = 15
+    filled_len = math.floor(bar_len * percent)
+    remainder = bar_len * percent - filled_len
+    if remainder >= 0.5:
+        bar = '█' * filled_len + '▒' + '░' * (bar_len - filled_len -1)
+    else: 
+        bar = '█' * filled_len + '░' * (bar_len - filled_len)
+    return bar
+
+#stolen progress bar function, each character represents 8 blocks, each block represents 100/96 percent
+#doesn't look good in telegram
+def progress12(percent):
+    block_len = 96
+    bar_len = 12
+    eighths = [' ', '▏', '▎ ', '▍', '▌', '▋', '▊', '▉', '█']
+    full_blocks = math.floor(bar_len * percent)
+    remainder = math.floor(block_len * percent) % 8
+    if full_blocks == bar_len:
+        bar = '[' + eighths[8] * full_blocks + ']'
+    else:
+        bar = '[' + eighths[8] * full_blocks + eighths[remainder] + ' ' * (bar_len - full_blocks - 1) + ']'
+    return bar
+
+#is weekend function
+def isweekend(now):
+    if now.weekday() < 4 :
+        return 0
+    elif now.weekday() == 4 and now.hour < 17 :
+        return 0
+    elif now.weekday() == 4 and now.hour > 16 :
+        return 1
+    else:
+        return 1
+    
+#days remaining function
+def days_remaining(now):
+    weekday = now.weekday()
+    if weekday > 4 :
+        days_remaining = 0
+    else:
+        days_remaining = 4 - now.weekday()
+    return days_remaining
+
+#hours remaining function
+def hours_remaining(now):
+    if now.hour < 8 :
+        dayhours_remaining = 9
+    elif now.hour > 17 :
+        dayhours_remaining = 0
+    else:
+        dayhours_remaining = 16 - now.hour
+    return dayhours_remaining
+
+#minutes remaining function
+def minutes_remaining(now):
+    if now.hour < 8 :
+        minutes_remaining = 0
+    elif now.hour > 17 :
+        minutes_remaining = 0
+    else:
+        minutes_remaining = 60 - now.minute
+    return minutes_remaining
+
+#tracker function
+def tracker():    
+    now = datetime.now()
+    # test time
+    #now = datetime(2023,4,27,16,14)
+    weekend = isweekend(now)
+    dailyhours = 9
+    hourlyminutes = 60
+    weeklyhours = 45
+
+    if weekend == 1 : 
+        week_percent = 1
+    else:
+        days = days_remaining(now)
+        hours = hours_remaining(now)
+        minutes = minutes_remaining(now)
+        week_percent = (1-(days*dailyhours+hours+minutes/hourlyminutes)/weeklyhours)
+    bar=progress15semishade(week_percent)
+    return "the week is "+str("{:.1f}".format(week_percent*100))+"% complete\n"+bar
 
 #respond time
 @bot.message_handler(commands=['time'])
@@ -27,47 +112,7 @@ def send_time(message):
 #respond tracker
 @bot.message_handler(commands=['tracker'])
 def send_time(message):
-    now = datetime.now()
-    #calc days remaining
-    weekday = now.weekday()
-    if weekday > 4 :
-        days_remaining = 0
-    else:
-        days_remaining = 4 - now.weekday()
-    #calc weekend
-    if now.weekday() < 4 :
-        weekend = 0
-    elif now.weekday() == 4 and now.hour < 17 :
-        weekend = 0
-    elif now.weekday() == 4 and now.hour > 16 :
-        weekend = 1
-    else:
-        weekend = 1
-    #calc hours remaining in current day
-    if now.hour < 8 :
-        dayhours_remaining = 9
-    elif now.hour > 17 :
-        dayhours_remaining = 0
-    else:
-        dayhours_remaining = 16 - now.hour
-    #calc minutes
-    if now.hour < 8 :
-        minutes_remaining = 0
-    elif now.hour > 17 :
-        minutes_remaining = 0
-    else:
-        minutes_remaining = 60 - now.minute
-    #calc hours remaining in week
-    if weekend == 1 : 
-        week_percent = 100
-    else:
-        week_percent = (1-(days_remaining*9+dayhours_remaining+minutes_remaining/60)/45)*100
-    if weekend == 1 :
-        bot.reply_to(message, "the week is 100% complete\n██████████")
-    else:
-        bar=progress(week_percent/100)
-        response = "the week is "+str("{:.1f}".format(week_percent))+"% complete\n"+bar
-        bot.reply_to(message, response)
+    bot.reply_to(message, tracker())
 
 #respond echo
 @bot.message_handler(func=lambda msg: True)
